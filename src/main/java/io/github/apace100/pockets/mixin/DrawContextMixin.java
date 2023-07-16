71e7ae5af92b29f6ce62632c2058fc677fbced70
Apace <49096778+apace100@users.noreply.github.com>
2023-07-16 03:51:01 +0200

    Updated to 1.20

--- src/main/java/io/github/apace100/pockets/mixin/DrawContextMixin.java
+++ src/main/java/io/github/apace100/pockets/mixin/DrawContextMixin.java
@@ -3,21 +3,23 @@ package io.github.apace100.pockets.mixin;
 import com.mojang.blaze3d.systems.RenderSystem;
 import io.github.apace100.pockets.PocketUtil;
 import net.minecraft.client.font.TextRenderer;
-import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.render.item.ItemRenderer;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ItemRenderer.class)
-public abstract class ItemRendererMixin {
+@Mixin(DrawContext.class)
+public abstract class DrawContextMixin {
 
-    @Inject(method = "renderGuiItemOverlay(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isItemBarVisible()Z"))
-    private void renderPocketBar(MatrixStack matrices, TextRenderer textRenderer, ItemStack stack, int x, int y, String countLabel, CallbackInfo ci) {
+    @Shadow public abstract void fill(RenderLayer layer, int x1, int y1, int x2, int y2, int color);
+
+    @Inject(method = "drawItemInSlot(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/item/ItemStack;IILjava/lang/String;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isItemBarVisible()Z"))
+    private void renderPocketBar(TextRenderer textRenderer, ItemStack stack, int x, int y, String countOverride, CallbackInfo ci) {
         if(!PocketUtil.hasPockets(stack)) {
             return;
         }
@@ -35,8 +37,8 @@ public abstract class ItemRendererMixin {
         int j = MathHelper.packRgb(0.4f, 0.4f, 1.0f);
         int drawX = x + 2;
         int drawY = y + 13 + yOffset;
-        DrawableHelper.fill(matrices, drawX, drawY, drawX + 13, drawY + 2, -16777216);
-        DrawableHelper.fill(matrices, drawX, drawY, drawX + i, drawY + 1, j | -16777216);
+        this.fill(RenderLayer.getGuiOverlay(), drawX, drawY, drawX + 13, drawY + 2, -16777216);
+        this.fill(RenderLayer.getGuiOverlay(), drawX, drawY, drawX + i, drawY + 1, j | -16777216);
         RenderSystem.enableDepthTest();
     }
 }
